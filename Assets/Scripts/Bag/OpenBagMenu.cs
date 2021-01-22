4d3ea8bef4e5254ec96cd50b48a7bb9bfcc0c410
Temmie <YunaDragoon@hotmail.com>
2021-01-22 09:36:02 +0100

    Step 1 of adding the bag open menu

---
+++ Assets/Scripts/Bag/OpenBagMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Button;
+
+public class OpenBagMenu : MonoBehaviour
+{
+    [SerializeField] private Button _Bag;
+    [SerializeField] private GameObject _BagInterface;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = _Bag.GetComponent<Button>();
+        btn.OnClick.AddListener(OpenBag);
+    }
+
+    void OpenBag()
+    {
+        instantiate(_BagInterface, new Vector3(0, 0, 0));
+    }
+}
